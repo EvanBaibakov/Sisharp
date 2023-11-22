9a2c597406e102c7c2347a869698a101bc263322
EvanBaibakov <8090500@mail.ru>
2023-11-22 19:12:25 +0300

    zafiksili

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,6 @@
-﻿int numberA = new Random().Next(2,10); //1234....9
+﻿int numberA = new Random().Next(1,10); //1234....9
 Console.WriteLine(numberA);
-int numberB = new Random().Next(2,10); //12345....9
+int numberB = new Random().Next(1,10); //12345....9
 Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
